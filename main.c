b1ec4f64c82bd883391dc12746311bdd7f8ca35e
JulesNyom <julesnyom@gmail.com>
2024-12-04 18:39:01 +0100

    Added my age

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main ()
   char characterName[] = "Julio";
   int characterAge = 27;
   printf("The main character is %s\n", characterName);
+  printf("his age is %d years old", characterAge);
   return 0;
 }
