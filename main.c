ce80b577bb18d9060afd449d60c23f86f86404b5
JulesNyom <julesnyom@gmail.com>
2024-12-04 18:48:42 +0100

    Created floating and double variables

--- main.c
+++ main.c
@@ -3,6 +3,8 @@ int main ()
 {
   char characterName[] = "Julio";
   int characterAge = 27;
+    float s = 6.7;
+    double m = 9.137838;
   printf("The main character is %s\n", characterName);
   printf("his age is %d years old\n", characterAge);
   return 0;
