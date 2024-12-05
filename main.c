da41dda8b9d7b1df7fcd4e1c623d58e765d0e2f4
JulesNyom <julesnyom@gmail.com>
2024-12-05 07:49:59 +0100

    Tried to modifiy my format specifier

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ int main ()
     float s = 6.7;
     double m = 9.137838;
     long long int a = 123938392020;
-  printf("The speed of light is %lld\n", a);
+  printf("The speed of light is %2.lld\n", a);
   printf("The main character is %s\n", characterName);
-  printf("his age is %d years old\n", characterAge);
+  printf("his age is %2.d years old\n", characterAge);
   return 0;
 }
