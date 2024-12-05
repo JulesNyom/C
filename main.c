0b6cf09b3dc212b743adf263bbf700ca5fa274df
JulesNyom <julesnyom@gmail.com>
2024-12-05 08:03:49 +0100

    understanding how to give inputs to my programs

--- main.c
+++ main.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 int main ()
 {
-  char characterName[] = "Julio";
-  int characterAge = 27;
-    float s = 6.7;
-    double m = 9.137838;
-    long long int a = 123938392020;
-  printf("The speed of light is %2.lld\n", a);
-  printf("The main character is %s\n", characterName);
-  printf("his age is %2.d years old\n", characterAge);
+  int age;
+  printf("How old are you?\n");
+  scanf("%d", &age);
+  printf("You are %d years old\n", age);
   return 0;
 }
