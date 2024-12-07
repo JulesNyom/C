884e2ff03ac33310fe70d366c85a0d6204255cb4
JulesNyom <julesnyom@gmail.com>
2024-12-07 22:14:01 +0100

    Created a function

--- main.c
+++ main.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
 #include <ctype.h>
 
+void Hello ()
+{
+  printf("Im a chill guy");
+}
+
 int main ()
 {
-char unit;
-float temp;
-
-unit = toupper(unit);
-
-printf("\nIs the temperature in (F) or (C)?: ");
-scanf("%c", &unit);
-
-if (unit == 'C') {
-  printf("The temperature is currently in C");
-}
-else if (unit == 'F')
-{
-  printf("The temperature is currently in F ");
-}
-else {
-printf("\n %c is not a valid measurement", unit);
-}
-
+  Hello();
   return 0;
 } 
