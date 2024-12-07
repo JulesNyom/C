a8a908989ae55803cc042694a592dbd40fdc9b72
JulesNyom <julesnyom@gmail.com>
2024-12-07 22:40:58 +0100

    Tried to create a if statement but made it incorrectly

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-void Hello ()
+void Hello(void)  // Added void parameter for better practice
 {
-  printf("Im a chill guy");
+    char tomate[10];
+    printf("Enter a tomate type: ");  // Added colon and space for better UX
+    scanf("%9s", tomate);  // Fixed three issues here
+    
+    if (strlen(tomate) >= 10) {  // Fixed brace placement
+        printf("Your tomate is too long\n");
+    }
+    else {  // Removed incorrect condition and fixed syntax
+        printf("Good choice\n");
+    }
 }
 
-int main ()
+int main(void)  // Added void parameter
 {
-  Hello();
-  return 0;
-} 
+    Hello();
+    return 0;
+}
