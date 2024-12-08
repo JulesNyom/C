e35c54efbc1513fb62ebdf615ef8b2c82272acf9
JulesNyom <julesnyom@gmail.com>
2024-12-08 10:22:20 +0100

    Still creating my calculator

--- main.c
+++ main.c
@@ -3,14 +3,37 @@
 #include <string.h>
 
 double calculator (double a , double b)
-{
+{ 
+  char operator;
   double a;
   double b;
 
+  scanf("%s\n", &operator);
+  printf("%s\n", "Enter an operator" );
   scanf("%f\n", &a);
+  printf("%s\n", "Enter your first number");
   scanf("%f\n", &b);
+  printf("%s\n", "Enter your second number");
 
-  
+  if (operator = "+") {
+    double result = a + b;
+    printf("%f/n", result);
+  }
+
+  else if (operator = "-") {
+    double result = a - b;
+    printf("%f/n", result);
+  }
+
+  else if (operator = "/") {
+    double result = a / b;
+    printf("%f/n", result);
+  }
+
+  else (operator = "x") {
+    double result = a * b;
+    printf("%f/n", result);
+  }
 
 }
 
