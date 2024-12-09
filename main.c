15ad93cce13df7f4a49db5c8c07c14ffa819ba36
JulesNyom <julesnyom@gmail.com>
2024-12-09 20:16:01 +0100

    Having some fun with c

--- main.c
+++ main.c
@@ -4,15 +4,7 @@
 
 int main(void) {
 
-  char name[100];
-  char favoriteFood[100];
-
-  printf("Whats your name ?: ");
-  scanf("%s", name);
-  printf("Whats your favorite food ?: ");
-  scanf("%s", favoriteFood);
-
-  printf("Hello my name is %s and i love %s", name, favoriteFood);
+  int number;
 
     return 0;
 }
