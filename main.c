fc4927f2dd9306ecd7ec9a2a12293f8979bae906
JulesNyom <julesnyom@gmail.com>
2024-12-09 20:34:53 +0100

    Improved my code

--- main.c
+++ main.c
@@ -2,24 +2,21 @@
 #include <ctype.h>
 #include <string.h>
 
-double math (double number, double numbertwo) {
-  
-  printf("Enter a number: ");
-  scanf("%lf", &number);
-
-  printf("Enter a second number: ");
-  scanf("%lf", &numbertwo);
-
-  double result = number + numbertwo;
-
-  printf("%lf", result);
-
-  return 0;
+double math(double number, double numbertwo) {
+    return number + numbertwo;  // Actually use the parameters
 }
 
 int main(void) {
-
-    math(200, 300);
-
+    double num1, num2;
+    
+    printf("Enter a number: ");
+    scanf("%lf", &num1);
+    
+    printf("Enter a second number: ");
+    scanf("%lf", &num2);
+    
+    double result = math(num1, num2);
+    printf("%.2lf\n", result);  // Added .2 to limit decimal places and \n for newline
+    
     return 0;
 }
