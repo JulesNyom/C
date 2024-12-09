004e31a6397d7ad2a1d9c2d37ef8ac8951c68a35
JulesNyom <julesnyom@gmail.com>
2024-12-09 20:21:40 +0100

    Trying to do simple math operations

--- main.c
+++ main.c
@@ -2,9 +2,20 @@
 #include <ctype.h>
 #include <string.h>
 
+double math (double number, double numbertwo) {
+  
+  printf("Enter a number");
+  scanf("%d", number);
+
+  printf("Enter a second number");
+  scanf("%d", numbertwo);
+
+  return 0;
+}
+
 int main(void) {
 
-  int number;
+    math(200, 300);
 
     return 0;
 }
