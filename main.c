77dfd2bad6280aa1a800d6c1beaccfdfbf946d83
JulesNyom <julesnyom@gmail.com>
2024-12-09 20:03:46 +0100

    almost finalised the string

--- main.c
+++ main.c
@@ -7,6 +7,11 @@ int main(void) {
   char name[100];
   char favoriteFood[100];
 
+  printf("Whats your name ?");
+  scanf("%s", name);
+  printf("Whats your favorite food ?");
+  scanf("%s", favoriteFood);
+
   printf("Hello my name is Jules and i love");
 
     return 0;
