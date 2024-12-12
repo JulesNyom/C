08f4afd732fe9fc908b9ff9995fe4e3255c4dcbd
JulesNyom <julesnyom@gmail.com>
2024-12-12 17:33:39 +0100

    Added the choosen variable

--- main.c
+++ main.c
@@ -4,10 +4,13 @@
 
 int main(void) {
    
+   char choosenLetter;
    char findALetter[][100] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-   char chooseANumber[][100] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"};
+   char choseANumber[][100] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"};
     
+
     printf("Please choose a number and we will find the corresponding letter");
+    scanf("%c", choosenLetter);
 
     return 0;
 }  
