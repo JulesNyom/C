5ccb7c9436b43f00a98e98e0f89f16181b23f2d4
JulesNyom <julesnyom@gmail.com>
2024-12-15 12:48:28 +0100

    Tested my algorithm

--- Test.py
+++ Test.py
@@ -1,3 +1,5 @@
+import random
+
 def BinarySearch(arr, target):
     left = 0
     right = len(arr) - 1
@@ -15,6 +17,10 @@ def BinarySearch(arr, target):
 
     return -1
 
+Sorted_array = [10, 55, 89954, 365, 484, 9455, 99, 95, 655, 559, 655, 587, 440]
+target = 55
+
+print(BinarySearch(Sorted_array, target))
         
 
 
