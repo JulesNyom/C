a0e077d7cb44d4271345691d29905290f273a094
JulesNyom <julesnyom@gmail.com>
2024-12-15 14:44:18 +0100

    Correctly implemented a binary search algorithm for the first time

--- Test.py
+++ Test.py
@@ -1,22 +1,25 @@
-def binarysearch (arr, target):
+def binarysearch (array, target):
     left = 0
-    right = len(arr) -1
+    right = len(array) - 1 
 
     while (left <= right):
-        mid = (left + right) // 2
+        mid = (left + right) // 2 
 
-        if target == arr[mid]:
+        if target == array[mid]:
             return mid
         
-        elif target > arr[mid]:
-            left = mid + 1
+        elif target < array[mid]:
+            right = mid - 1
 
-        else: right = mid - 1
+        else : left = mid + 1
 
     return -1 
 
+
 Array = [12, 98, 87, 32, 455, 265, 78, 3565, 656]
-target = 98
+Array.sort()
+print(Array)
+target = 656
 
 print(binarysearch(Array, target))
 
