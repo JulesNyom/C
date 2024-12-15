f715cff9b1245069b05dd8c9e71543d8078e5e62
JulesNyom <julesnyom@gmail.com>
2024-12-15 13:23:39 +0100

    Corrected my binary search

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 def binarysearch (arr, target):
     left = 0
-    right = len(arr) - 1
+    right = len(arr) -1
 
     while (left <= right):
         mid = (left + right) // 2
@@ -8,15 +8,15 @@ def binarysearch (arr, target):
         if target == arr[mid]:
             return mid
         
-        elif target < mid:
-            right = mid - 1
+        elif target > arr[mid]:
+            left = mid + 1
 
-        else: left = mid + 1
+        else: right = mid - 1
 
     return -1 
 
 Array = [12, 98, 87, 32, 455, 265, 78, 3565, 656]
-target = 78
+target = 98
 
 print(binarysearch(Array, target))
 
