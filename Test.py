e7fa080d73f4d9990188f3371abe98ab5bd18377
JulesNyom <julesnyom@gmail.com>
2024-12-16 00:01:16 +0100

    Practiced my binary search logic

--- Test.py
+++ Test.py
@@ -1,31 +1,23 @@
-def binarysearch (array, target):
+def binarysearch (arr, target):
     left = 0
-    right = len(array) - 1 
+    right = len(arr) - 1
 
     while (left <= right):
         mid = (left + right) // 2
 
-        if target == array[mid]:
-            return mid 
+        if target == arr[mid]:
+            return mid
         
-        elif target < array[mid]:
-            right = mid - 1 
+        elif target < arr[mid]:
+            right = mid - 1
 
-        else: left = mid + 1 
+        else: left = mid + 1
 
-    return -1 
+    return -1
 
+array = [ 20, 8392, 2300, 1398, 38832, 20939, 339938, 287338]
+array.sort()
+print(array)
+newtarget = 20
 
-
-
-Array = [12, 98, 87, 32, 455, 265, 78, 3565, 656]
-Array.sort()
-print(Array)
-target = 12
-
-print(binarysearch(Array, target))
-
-    
-        
-
-
+print(binarysearch(array, newtarget))
