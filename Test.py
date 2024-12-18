223cd843daa77b0576046b384f90741b2b985551
JulesNyom <julesnyom@gmail.com>
2024-12-18 19:19:22 +0100

    Successfully implemented all algos

--- Test.py
+++ Test.py
@@ -1,10 +1,10 @@
-array = [33, 89, 55, 30, 23782, 374, 44, 3763, 44, 8733, 29, 737]
+Array = [64, 34, 25, 12, 22, 11, 90, 54]
 
 def binarysearch(array, target):
     left = 0
     right = len(array) - 1
 
-    while (left <= right):
+    while left <= right:
         mid = (left + right) // 2
 
         if target == array[mid]:
@@ -14,7 +14,7 @@ def binarysearch(array, target):
             right = mid - 1
         
         else: left = mid + 1
-
+    
     return -1
 
 def linearsearch(array, target):
@@ -23,18 +23,19 @@ def linearsearch(array, target):
             return i
     return -1
 
-def merge (left, right):
+def merge(left, right):
     result = []
-    i, j = 0, 0
+    i, j= 0, 0
+
     while i < len(left) and j < len(right):
-        if left[i] < right[j]:
+        if left[i] <= right[j]:
             result.append(left[i])
             i += 1
         else: 
             result.append(right[j])
             j += 1
-    result += left[i:]
-    result += right[j:]
+    result+=left[i:]
+    result+=right[j:]
     return result
 
 def mergesort(list):
@@ -43,10 +44,11 @@ def mergesort(list):
     mid = len(list) // 2
     left = mergesort(list[:mid])
     right = mergesort(list[mid:])
-    return merge(left, right)
+    result = merge(left,right)
+    return result
 
-print(mergesort(array))
-print(binarysearch(array, 55))
-print(linearsearch(array, 55))
+print(mergesort(Array))
+print(binarysearch(Array, 30))
+print(linearsearch(Array, 11))
 
 
