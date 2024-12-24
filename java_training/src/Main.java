bc555ade255a706bea8385a2d70b3fdd9a9feb9e
JulesNyom <julesnyom@gmail.com>
2024-12-24 14:51:12 +0100

    Created variables

---
+++ java_training/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {  
+
+    public static void main(String[] args) { 
+        double hello = 5.0;
+        boolean b = true;
+        char c = 'b';
+        String coucou = "coucou la miff";
+        System.out.println(hello);    
+    }
+}
