4107937d6142b36a9fabcda7653ced643d26451a
JulesNyom <julesnyom@gmail.com>
2024-12-24 11:17:42 +0100

    Implemented my daily algos

--- Test.py
+++ Test.py
@@ -1,28 +1,30 @@
-def binarysearch(arr, target):
+def binarysearch(array, target):
     left = 0
-    right = len(arr) - 1
+    right = len(array) - 1
 
     while left <= right:
         mid = (left + right) // 2
-        if target == arr[mid]:
+        if target == array[mid]:
             return mid
-        elif target <= arr[mid]:
+        elif target < array[mid]:
             right = mid - 1
-        else : left = mid + 1
+        else: left = mid + 1 
     return -1
 
-array = [1, 2, 3]
-print(binarysearch(array, 3))
+    # Create a sorted array with 20 integers
+
+sorted_array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+print(binarysearch(sorted_array, 11))
 
 def merge(left, right):
     result = []
-    i, j = 0, 0
+    i, j = 0, 0 
 
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
             result.append(left[i])
             i+=1
-        else:
+        else: 
             result.append(right[j])
             j+=1
     result+=left[i:]
@@ -32,9 +34,19 @@ def merge(left, right):
 def mergesort(list):
     if len(list) < 2:
         return list
-    mid = len(list) // 2
+    mid = len(list) // 2 
     left = mergesort(list[:mid])
     right = mergesort(list[mid:])
     return merge(left, right)
 
-arr2 = [7, 2, 9, 1, 5, 8, 3, 6, 4, 10]
+# Create an array with 10 unsorted integers
+unsorted_array = [42, 17, 8, 93, 25, 61, 3, 79, 50, 12]
+print(mergesort(unsorted_array))
+
+def linearsearch(array, target):
+    for i in range(len(array)):
+        if target == array[i]:
+            return i 
+    return -1 
+    
+print(linearsearch(unsorted_array, 17))
