112d23a7361e6576bfc81929f8ef8d7c41647fbe
JulesNyom <julesnyom@gmail.com>
2024-12-25 15:24:13 +0100

    I

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,11 +1,18 @@
-import java.util.Scanner;
-
-import javax.swing.JOptionPane;
 
 
 public class Main {  
 
     public static void main(String[] args) { 
-        
+        int age = 48;
+
+        if (age <= 30) {
+            System.out.println("you are a baby");
+        }
+        else if (age <= 75) {
+            System.out.println("Ok BOOMER !");
+        }
+        else {
+            System.out.println("you are a grown ass man");
+        };
     }
 }
