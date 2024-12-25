ddccfa8b2cfaa12d3fc836465f603127e28401eb
JulesNyom <julesnyom@gmail.com>
2024-12-25 15:13:17 +0100

    Starting from scratch

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 
 public class Main {  
 
     public static void main(String[] args) { 
-        Scanner scan = new Scanner(System.in);
-        System.out.println(scan);
+        
     }
 }
