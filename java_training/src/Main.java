81b54d08d98f1c8b94bf833bcf232bc575e29845
JulesNyom <julesnyom@gmail.com>
2024-12-25 21:06:15 +0100

    Implemented my first algo in java

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,18 +1,15 @@
-
-
 public class Main {  
-
-    public static void main(String[] args) { 
-        int age = 48;
-
-        if (age <= 30) {
-            System.out.println("you are a baby");
+    public static int linearSearch(int[] array, int target) { 
+        for (int i = 0; i < array.length; i++) {  // Changed <= to 
+            if (target == array[i]) {
+                return i;
+            }
         }
-        else if (age <= 75) {
-            System.out.println("Ok BOOMER !");
-        }
-        else {
-            System.out.println("you are a grown ass man");
-        };
+        return -1;
+    }
+
+    public static void main(String[] args) {  // Added main method
+        int[] numbers = {1, 2, 3, 4, 5, 6};
+        System.out.println(linearSearch(numbers, 20));
     }
 }
