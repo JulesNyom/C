2163cad34b24200516fdb5302972afad5221c259
JulesNyom <julesnyom@gmail.com>
2024-12-26 16:29:08 +0100

    starting from scratch

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,10 +1,4 @@
-import java.util.ArrayList;
-
 public class Main {
-    public static void main (String[] args) {
-        ArrayList<Integer> fishes = new ArrayList<>();
-        fishes.add(10);
-        System.out.println(fishes);
-    
-    }
+
+
 }
