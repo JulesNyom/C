8383dd0837ec87f0978e619cb3c341b9ece2d9ea
JulesNyom <julesnyom@gmail.com>
2024-12-26 08:05:48 +0100

    Did a simple ArrayList

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,16 +1,10 @@
+import java.util.ArrayList;
 
 public class Main {
-
-    static int binarysearch (int[] array, int target) {
-        for(int i=0; i<array.length; i++) {
-            if (target == array[i]) {
-                return i;
-            }
-        }
-        return -1;
-    }
     public static void main (String[] args) {
-        int[] sortedarray = {1, 2, 3, 4, 5, 6, 22, 43, 50};
-        System.out.println(binarysearch(sortedarray,22));
+        ArrayList<Integer> fishes = new ArrayList<>();
+        fishes.add(10);
+        System.out.println(fishes);
+    
     }
 }
