865dd7cf72c37b30b6ce229c524395d2342789b9
JulesNyom <julesnyom@gmail.com>
2024-12-26 07:53:12 +0100

    Starting to understand a bit java

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -2,21 +2,11 @@
 public class Main {
 
     static int binarysearch (int[] array, int target) {
-        int left = 0;
-        int right = array.length - 1;
-
-        while (left <= right) {
-          int mid = (left + right) / 2;
-          if (target == array[mid]) {
-            return mid;
-          }
-          else if (target <= array[mid]) {
-            right = mid - 1;
-          }
-          else {
-            left = mid + 1;
-          }
-        };
+        for(int i=0; i<array.length; i++) {
+            if (target == array[i]) {
+                return i;
+            }
+        }
         return -1;
     }
     public static void main (String[] args) {
