b91919c575cbf3f6da291fad2af3cd8a35eee05f
JulesNyom <julesnyom@gmail.com>
2024-12-26 17:08:53 +0100

    Created my first object in java

---
+++ java_training/src/car.java
@@ -0,0 +1,18 @@
+public class car {
+    
+    String model = "tesla";
+    String color = "black";
+    int horsepower = 403;
+
+    void park () {
+        System.out.println("The car is parked");
+    }
+    void drive () {
+        System.out.println("You are driving the car");
+
+    }
+    void stop() {
+        System.out.println("You stopped the car");
+
+    }
+}
