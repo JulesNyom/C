336196c157df14d053524c80ef7753222b5d092e
JulesNyom <julesnyom@gmail.com>
2024-12-26 16:40:10 +0100

    did a binarysearch

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,4 +1,30 @@
 public class Main {
 
+    static int binarysearch (int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == array[mid]) {
+                return mid;
+            }
+            else if (target <= array[mid]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    public static void main (String[] args) {
+        int[] sortedarray = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(binarysearch(sortedarray, 7));
+
+    }
 
 }
