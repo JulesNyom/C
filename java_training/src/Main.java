77f9630656d98e623323845bae033c72bfd01c38
JulesNyom <julesnyom@gmail.com>
2024-12-27 10:00:58 +0100

    Created my first array of objects

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,8 +1,18 @@
 public class Main {
     public static void main (String[] args) {
 
-        Code code = new Code("Praline", "Java");
-        System.out.println(code.food);
+        Code[] stockage = new Code[2];
+
+        Code code1 = new Code("Praline", "Java");
+        Code code2 = new Code("Chocolat blanc", "React");
+
+        stockage[0] = code1;
+        stockage[1] = code2;
+
+        System.out.println(stockage[0].language);
+
+        //Code[] stockage = {code1, code2};
+        
 
     }
 
