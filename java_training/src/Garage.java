d6c3b696f1ee383dfaff55261d3ef80f8cfdd8a3
JulesNyom <julesnyom@gmail.com>
2024-12-28 10:40:34 +0100

    did inheritance

--- java_training/src/Garage.java
+++
@@ -1,5 +0,0 @@
-public class Garage {
-    void park(Car car) {  
-        System.out.println("My " + car.name + " is " + car.color + " and parked");
-    }
-}
--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        Garage garage = new Garage();
-        Car car1 = new Car("blue", "Tesla");
-        garage.park(car1);
+
+   public static void main (String[] args) {
+        Room room = new Room();
+        Bathroom bathroom = new Bathroom();
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(room.binarysearch(arr, 6));
+        System.out.println(bathroom.binarysearch(arr, 8));
     }
+
 }
