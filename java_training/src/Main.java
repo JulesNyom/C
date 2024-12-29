2b7e1faca59f9f4f39a8889796785b725c4a6c71
JulesNyom <julesnyom@gmail.com>
2024-12-29 15:33:06 +0100

    Starting from scratch

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
 
-   public static void main (String[] args) {
-        Room room = new Room();
-        Bathroom bathroom = new Bathroom();
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(room.binarysearch(arr, 6));
-        System.out.println(bathroom.binarysearch(arr, 8));
-        System.out.println(bathroom.binarysearch(arr, 10));
+    public static void main (String[] args) {
+        
     }
-
 }
