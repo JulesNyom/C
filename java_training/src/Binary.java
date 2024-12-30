a21d7af0b307426be261766de5c244b6cd0cf0d2
JulesNyom <julesnyom@gmail.com>
2024-12-30 18:32:32 +0100

    Strating from scratch

--- java_training/src/Binary.java
+++
@@ -1,7 +0,0 @@
-public class Binary extends Algos {
-    
-    Binary (int mango) {
-        super(mango);
-    }
-
-}
--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
 
     public static void main (String[] args) {
-         binary = new Binary(3, 4);
-
-        System.out.println(binary.pasta);
+        
 
     }
 }
