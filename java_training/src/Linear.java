06f8b211b930ff806a7d72df98530f5a3816d635
JulesNyom <julesnyom@gmail.com>
2025-01-01 21:18:46 +0100

    Finished my linear algo

--- java_training/src/Linear.java
+++ java_training/src/Linear.java
@@ -1,3 +1,11 @@
 public class Linear {
-    
+    int linearsearch (int[] array, int target){
+        for (int i=0; i < array.length; i++) {
+            if (target == array[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
