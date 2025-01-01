c6bf3b5fcd043290492f34427fd989fa57767468
JulesNyom <julesnyom@gmail.com>
2025-01-01 21:07:31 +0100

    Creating a binary search algo

---
+++ java_training/src/Binary.java
@@ -0,0 +1,15 @@
+public class Binary {
+    
+    int binarysearch (int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == array[mid])
+        }
+
+
+        return -1;
+    }
+}
