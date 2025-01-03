91fc2efbe6c73f4c922c59f089f6b3bf9ffee0b7
JulesNyom <julesnyom@gmail.com>
2025-01-03 07:27:47 +0100

    Testing my binary serach

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        Binary binary = new Binary();
+        Linear linear = new Linear();
+
+        int [] array = {1,2,3,4,18,22,99};
+
+        binary.binarysearch(array, 4);
         
 
     }
