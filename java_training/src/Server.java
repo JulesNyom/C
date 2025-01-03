080f4d1a8aed0de70bf5acc311b5979336981ba5
JulesNyom <julesnyom@gmail.com>
2025-01-03 17:41:17 +0100

    Trying to understand the code related to the creation of a webserver

--- java_training/src/Server.java
+++ java_training/src/Server.java
@@ -1,3 +1,18 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
 public class Server {
-    
+
+    private HttpServer server;
+    private final int port;
+
+    public Server (int port) {
+        this.port = port;
+    }
+
+    public void start () throws IOException {
+        server = HttpServer.create(new InetSocketAddress(port),0);
+        server.createContext("/", new HomeHandler());
+    }
 }
