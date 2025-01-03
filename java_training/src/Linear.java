5f3094495bfd8a8db887aea2858bee6d2ef3b7d3
JulesNyom <julesnyom@gmail.com>
2025-01-03 07:24:14 +0100

    Modified the linearsearch

--- java_training/src/Linear.java
+++ java_training/src/Linear.java
@@ -1,11 +1,13 @@
 public class Linear {
-    int linearsearch (int[] array, int target){
+
+    int linearsearch (int[] array, int target) {
         for (int i=0; i < array.length; i++) {
             if (target == array[i]) {
                 return i;
             }
         }
-
         return -1;
+
     }
+
 }
