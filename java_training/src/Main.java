016a25eb77bfa6b4bc08449acf047a49a9871c99
JulesNyom <julesnyom@gmail.com>
2025-01-04 20:13:29 +0100

    Starting from scratch again

--- java_training/src/Main.java
+++ java_training/src/Main.java
@@ -1,9 +1,13 @@
 // Driver Class
+
+import java.io.IOException;
+
 public class Main
 {
     // Main Method
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-
+        Server server = new Server(8000);
+        server.start();
     }
 }
