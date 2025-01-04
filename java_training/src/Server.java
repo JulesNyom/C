dca885fec0d15a24d007b04ab667c5ecfc058097
JulesNyom <julesnyom@gmail.com>
2025-01-04 18:09:23 +0100

    So far, im pretty close to doing exactly the good iimplementation

--- java_training/src/Server.java
+++ java_training/src/Server.java
@@ -1,5 +1,17 @@
+import com.sun.net.httpserver.HttpServer;
+
 public class Server {
 
+    private HttpServer server;
+    private final int port;
+
+    public Server (int port) {
+        this.port = port;
+    }
+
+    public void start () {
+        
+    }
 
 
 }
