bfb23cfcbdf47b2c1f8c24a639dea2ee1ffbb42c
JulesNyom <julesnyom@gmail.com>
2025-01-05 09:07:01 +0100

    Starting implementation from scratch again

---
+++ java_training/src/Server.java
@@ -0,0 +1,12 @@
+import com.sun.net.httpserver.HttpServer;
+
+public class Server {
+    private HttpServer server;
+    private final int port;
+
+    public Server (int port) {
+        this.port = port;
+    }
+
+    
+}
