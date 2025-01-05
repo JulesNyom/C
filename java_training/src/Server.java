f18c93c9896ad71a9f2da7344fd05685b6b6bcfa
JulesNyom <julesnyom@gmail.com>
2025-01-05 09:13:44 +0100

    Implemented the first of the server

--- java_training/src/Server.java
+++ java_training/src/Server.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Server {
@@ -8,5 +12,16 @@ public class Server {
         this.port = port;
     }
 
-    
+    public void start() throws IOException {
+        server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", null);
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server is running on port 8000");
+    }
+
+    static class HomeHandler implements HttpHandler {
+        
+    }
+
 }
