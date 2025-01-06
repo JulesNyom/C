c9dc84a88f3c4400ff39b3f0b9df29ba93720174
JulesNyom <julesnyom@gmail.com>
2025-01-06 17:57:41 +0100

    Implemented a part of merge sort in java

--- java_training/src/Algos.java
+++ java_training/src/Algos.java
@@ -1,57 +1,36 @@
 public class Algos {
     
-    public static int[] merge(int[] left, int[] right) {
-        int[] result = new int[left.length + right.length];
-        int i = 0, j = 0, k = 0;
-        
-        while (i < left.length && j < right.length) {
-            if (left[i] <= right[j]) {
-                result[k++] = left[i++];
-            } else {
-                result[k++] = right[j++];
+    static void mergeSort(int[] array) {
+        int length = array.length;
+        if (length <= 1) {
+            return; // base case
+        }
+
+        int mid = length / 2;
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[length - mid];
+
+        int i = 0;
+        int j = 0;
+
+        for (; i<length; i++) {
+            if (i < mid) {
+                leftArray[i] = array[i];
+            }
+            else {
+                rightArray[j] = array[i];
+                j++;
             }
         }
-        
-        // Copy remaining elements from left array, if any
-        while (i < left.length) {
-            result[k++] = left[i++];
-        }
-        
-        // Copy remaining elements from right array, if any
-        while (j < right.length) {
-            result[k++] = right[j++];
-        }
-        
-        return result;
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, array);
     }
 
-    public static int[] mergeSort(int[] list) {
-        if (list.length < 2) {
-            return list;
-        }
-        
-        int middle = list.length / 2;
-        
-        // Create left and right subarrays
-        int[] left = new int[middle];
-        int[] right = new int[list.length - middle];
-        
-        // Fill left subarray
-        for (int i = 0; i < middle; i++) {
-            left[i] = list[i];
-        }
-        
-        // Fill right subarray
-        for (int i = middle; i < list.length; i++) {
-            right[i - middle] = list[i];
-        }
-        
-        // Recursive calls
-        left = mergeSort(left);
-        right = mergeSort(right);
-        
-        return merge(left, right);
+    static void merge(int[] leftArray, int[] rightArray, int[] array) {
+       
     }
+
 
     public int binarysearch(int[] array, int target) {
         int left = 0;
