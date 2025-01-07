83a20b3a10be7c7f61540da1f2460f2519250b66
JulesNyom <julesnyom@gmail.com>
2025-01-07 22:13:29 +0100

    Implemented my daily mergesort in python

--- Test.py
+++ Test.py
@@ -6,10 +6,9 @@ def merge(left, right):
         if left[i] <= right[j]:
             result.append(left[i])
             i+=1
-        else : 
+        else: 
             result.append(right[j])
             j+=1
-
     result += left[i:]
     result += right[j:]
     return result
@@ -22,29 +21,6 @@ def mergesort(list):
     right = mergesort(list[mid:])
     return merge(left, right)
 
-unsorted_array = [34, 7, 23, 32, 5, 62, 78, 1, 13, 9]
+unsorted = [64, 34, 25, 12, 22, 11, 90, 10, 21, 5]
 
-sortedarray = (mergesort(unsorted_array))
-
-def binarysearch(array, target):
-    left = 0
-    right = len(array) -1 
-
-    while left < right:
-        mid = (left + right) // 2
-        if target == array[mid]:
-            return mid
-        elif target <= array[mid]:
-            right = mid -1
-        else : left = mid + 1
-    return -1 
-
-print(binarysearch(sortedarray, 34))
-
-def linearsearch(array, target):
-    for i in range(len(array)):
-        if target == array[i]:
-            return i
-    return -1
-
-print(linearsearch(unsorted_array, 34))
+print(mergesort(unsorted))
