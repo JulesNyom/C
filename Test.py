4ec44c3c9ff0ea6ddd1619e08df8d16c4b821c09
JulesNyom <julesnyom@gmail.com>
2025-01-08 07:02:10 +0100

    Did my daily mergesort algo un python

---
+++ Test.py
@@ -0,0 +1,25 @@
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i+=1
+        else: 
+            result.append(right[j])
+            j+=1
+    result+=left[i:]
+    result+=right[j:]
+    return result
+def mergesort(list):
+    if len(list) < 2:
+        return list
+    mid = len(list) // 2
+    left = mergesort(list[:mid])
+    right = mergesort(list[mid:])
+    return merge(left, right)
+
+unsorted = [9, 5, 1, 4, 3, 8, 6, 2, 7, 0]
+
+print(mergesort(unsorted))
