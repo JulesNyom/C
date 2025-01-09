fc72a7ee0cb6d8225903bf00b883999cb992020b
JulesNyom <julesnyom@gmail.com>
2025-01-09 06:51:47 +0100

    Did my dail linear and binary algo

--- java_training/src/Algos.java
+++ java_training/src/Algos.java
@@ -32,10 +32,10 @@ public class Algos {
     }
     public int binarysearch (int[] array, int target) {
         int left = 0;
-        int right = array.length;
+        int right = array.length - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (target == array[mid]) {
+            if (target == array[mid] ) {
                 return mid;
             }
             else if (target <= array[mid]) {
@@ -48,15 +48,14 @@ public class Algos {
         return -1;
     }
 
-   public int linearsearch (int[] array, int target) {
-    for (int i=0; i<array.length; i++) {
-        if (target == array[i]) {
-            return i;
+    public int linearsearch (int[] array, int target) {
+        for (int i = 0; i<array.length; i++) {
+            if (target == array[i]) {
+                return i;
+            }
         }
+        return -1;
     }
-
-    return -1;
-   }
     
 
 }
