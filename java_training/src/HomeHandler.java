ea83486d2730820da7702a43ec0f0a0ef0a6fc69
JulesNyom <julesnyom@gmail.com>
2025-01-09 06:57:15 +0100

    Started my server implementation

--- java_training/src/HomeHandler.java
+++ java_training/src/HomeHandler.java
@@ -1,16 +1,3 @@
-import java.io.IOException;
-import java.io.OutputStream;
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-
-public class HomeHandler implements HttpHandler {
-    public void handle(HttpExchange exchange) throws IOException {
-        String response = "Say hello";
-        exchange.sendResponseHeaders(200, response.length());
-        try (OutputStream os = exchange.getResponseBody() ) {
-            os.write(response.getBytes());
-        }
-    }
-
+public class HomeHandler {
+    
 }
