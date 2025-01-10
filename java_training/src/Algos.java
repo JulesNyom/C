a50bec21cf64f3b8ba81fcb2d4f57a4df768cebe
JulesNyom <julesnyom@gmail.com>
2025-01-10 22:51:16 +0100

    Started my binary search algo

--- java_training/src/Algos.java
+++ java_training/src/Algos.java
@@ -30,32 +30,17 @@ public class Algos {
     static void merge(int[] leftArray, int[] rightArray, int[] array) {
        
     }
+  
     public int binarysearch (int[] array, int target) {
         int left = 0;
         int right = array.length - 1;
+
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (target == array[mid] ) {
-                return mid;
-            }
-            else if (target <= array[mid]) {
-                mid = right - 1;
-            }
-            else {
-                mid = left + 1;
-            }
-        }
-        return -1;
-    }
 
-    public int linearsearch (int[] array, int target) {
-        for (int i = 0; i<array.length; i++) {
-            if (target == array[i]) {
-                return i;
-            }
         }
+
         return -1;
     }
-    
 
 }
