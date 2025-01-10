3662dc1e5456b013f2075a5a5cfc97bfea6a23b4
JulesNyom <julesnyom@gmail.com>
2025-01-10 23:02:44 +0100

    Did my linear search

--- java_training/src/Algos.java
+++ java_training/src/Algos.java
@@ -37,9 +37,27 @@ public class Algos {
 
         while (left <= right) {
             int mid = (left + right) / 2;
+            if (target == array[mid]) {
+                return mid;
+            }
+            else if (target <= array[mid]) {
+                mid = right - 1;
+            }
+            else {
+                mid = left + 1;
+            }
 
         }
 
+        return -1;
+    }
+
+    public int linearsearch (int[] array, int target) {
+        for (int i=0; i<array.length; i++) {
+            if (target == array[i]) {
+                return i;
+            }
+        }
         return -1;
     }
 
