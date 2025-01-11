42cc4fea3f79464660b880f9e5c22ab0c4188dd9
JulesNyom <julesnyom@gmail.com>
2025-01-11 17:26:06 +0100

    Did my daily binary and linear search algo

--- java_training/src/Algos.java
+++ java_training/src/Algos.java
@@ -32,33 +32,28 @@ public class Algos {
     }
   
     public int binarysearch (int[] array, int target) {
+
         int left = 0;
         int right = array.length - 1;
-
+        
         while (left <= right) {
             int mid = (left + right) / 2;
             if (target == array[mid]) {
                 return mid;
             }
             else if (target <= array[mid]) {
-                mid = right - 1;
+                mid = right -1;
             }
-            else {
-                mid = left + 1;
-            }
-
+            else { mid = left +1; }
         }
-
         return -1;
     }
-
     public int linearsearch (int[] array, int target) {
-        for (int i=0; i<array.length; i++) {
+        for (int i=0; i<=array.length; i++) {
             if (target == array[i]) {
                 return i;
             }
         }
         return -1;
     }
-
 }
