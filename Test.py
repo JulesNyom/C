25dd848602ac73440611b98f06d226d69e9791db
JulesNyom <julesnyom@gmail.com>
2025-01-12 17:14:53 +0100

    Perfectly did my daily mergesort algo

--- Test.py
+++ Test.py
@@ -1,17 +1,17 @@
 def merge(left, right):
     result = []
-    i, j = 0, 0
-
+    i, j = 0,0
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
             result.append(left[i])
             i+=1
-        else: 
+        else : 
             result.append(right[j])
             j+=1
     result+=left[i:]
     result+=right[j:]
     return result
+
 def mergesort(list):
     if len(list) < 2:
         return list
@@ -20,6 +20,5 @@ def mergesort(list):
     right = mergesort(list[mid:])
     return merge(left, right)
 
-unsorted = [9, 5, 1, 4, 3, 8, 6, 2, 7, 0]
-
+unsorted = [9, 5, 1, 4, 3, 8, 2, 7, 6, 0]
 print(mergesort(unsorted))
