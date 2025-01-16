543fb36902ce90ecd7d49e13669af51ee09b965e
JulesNyom <julesnyom@gmail.com>
2025-01-16 08:13:34 +0100

    Binary

--- java_training/src/Algos.java
+++ java_training/src/Algos.java
@@ -33,27 +33,22 @@ public class Algos {
 
     public int binarysearch (int[] array, int target) {
         int left = 0;
-        int right = array.length - 1;
+        int right = array.length -1;
 
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = (left + right) /2;
             if (target == array[mid]) {
                 return mid;
             }
             else if (target <= array[mid]) {
                 mid = right - 1;
             }
-            else {mid = left +1;}
-        }
-        return -1;
-    }
-
-    public int linearsearch ( int[] array, int target) {
-        for (int i=0; i<array.length; i++) {
-            if (target == array[i]) {
-                return i;
+            else {
+                mid = left + 1;
             }
         }
         return -1;
     }
+
+    public int linearsearch 
 }
