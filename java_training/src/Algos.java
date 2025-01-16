f61977badcfeac90165f8f37d09c386a6752493f
JulesNyom <julesnyom@gmail.com>
2025-01-16 08:15:45 +0100

    linear

--- java_training/src/Algos.java
+++ java_training/src/Algos.java
@@ -50,5 +50,12 @@ public class Algos {
         return -1;
     }
 
-    public int linearsearch 
+    public int linearsearch (int[] array, int target) {
+        for (int i = 0; i<array.length; i++) {
+            if (target == array[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
