ce8ba59f73fa87d2876a805af5aa4bd1537ef5c8
JulesNyom <julesnyom@gmail.com>
2025-01-19 17:43:39 +0100

    Added recusrive merge sort algorithm in java

---
+++ java_training/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    
+    static void mergeSort (int[] arr) {
+        if (arr == null || arr.length <=1) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right  = new int[arr.length - mid];
+
+    }
+}
