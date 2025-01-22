b5bfd69c980a321323833824112285242478933e
JulesNyom <julesnyom@gmail.com>
2025-01-22 07:59:36 +0100

    Server in plain java

--- java_training/src/HomeHandler.java
+++ java_training/src/HomeHandler.java
@@ -7,11 +7,10 @@ import com.sun.net.httpserver.HttpHandler;
 public class HomeHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String response = "Hello boy";
+        String response = "Hello World!";
         exchange.sendResponseHeaders(200, response.length());
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(response.getBytes());
         }
     }
-    
 }
