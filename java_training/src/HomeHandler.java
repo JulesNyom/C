84187484564791d1f75b59e5cd1243c17aa4bfdc
JulesNyom <julesnyom@gmail.com>
2025-01-24 07:05:42 +0100

    Created my daily server

--- java_training/src/HomeHandler.java
+++ java_training/src/HomeHandler.java
@@ -5,12 +5,14 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class HomeHandler implements HttpHandler {
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String response = "Hello World!";
+        String response = "Hello friends!";
         exchange.sendResponseHeaders(200, response.length());
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(response.getBytes());
         }
     }
+    
 }
--- java_training/src/Server.java
+++ java_training/src/Server.java
@@ -2,21 +2,20 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.*;
-
 public class Server {
 
     private HttpServer server;
     private final int port;
 
-    public Server(int port) {
+    public Server (int port) {
         this.port = port;
-    }
+    } 
 
-    public void start () throws IOException{
+    public void start () throws IOException {
         server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/", new HomeHandler());
         server.setExecutor(null);
         server.start();
-        System.out.println("Server is runnning on port 8000");
+        System.out.println("Server is running");
     }
 }
