79b3127f91f4b7bb409406b55546a3e29ce6b511
JulesNyom <julesnyom@gmail.com>
2025-01-25 15:24:55 +0100

    mergesort

--- Test.py
+++ Test.py
@@ -1,14 +1,13 @@
 def merge(left, right):
-    result =[]
+    result = []
     i, j = 0, 0
-
-    while i < len(left) and j < len(right):
+    while i< len(left) and j< len(right):
         if left[i] <= right[j]:
             result.append(left[i])
             i+=1
-        else :  
+        else : 
             result.append(right[j])
-            j+=1 
+            j+=1
     result+=left[i:]
     result+=right[j:]
     return result 
@@ -16,10 +15,11 @@ def merge(left, right):
 def mergesort(list):
     if len(list) < 2:
         return list
-    mid = len(list) // 2
+    mid = len(list) // 2;
     left = mergesort(list[:mid])
     right = mergesort(list[mid:])
     return merge(left, right)
 
-arr = [9, 5, 1, 4, 3, 8, 6, 2, 7, 0]
-print(mergesort(arr))
+unsorted = [64, 34, 25, 12, 22, 11, 90, 75, 5, 1]
+
+print(mergesort(unsorted))
