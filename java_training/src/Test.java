efe95c17bdc30cb1c58d7e097b77bdef412cc753
JulesNyom <julesnyom@gmail.com>
2025-01-28 06:21:43 +0100

    Making merge sort in java

---
+++ java_training/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+    public void mergeSort(int[] array) {
+        int length = array.length;
+
+        if (length <= 1) return;
+
+        int mid = length / 2;
+
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[length - mid];
+
+    }
+    
+}
