8a81da73ccf4a399cc3fa44be9b6d2e78835881d
JulesNyom <julesnyom@gmail.com>
2025-01-28 06:35:28 +0100

    Finished merge sort

--- java_training/src/Test.java
+++ java_training/src/Test.java
@@ -10,6 +10,38 @@ public class Test {
         int[] leftArray = new int[mid];
         int[] rightArray = new int[length - mid];
 
+        for(int i = 0; i < mid; i++) {
+            leftArray[i] = array[i];
+        }
+
+        for(int i = mid; i < length; i++) {
+            rightArray[i - mid] = array[i];
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, array);
     }
     
+    public void merge(int[] leftArray, int[] rightArray, int[] array) {
+        int leftSize = leftArray.length;
+        int rightSize = rightArray.length;
+        int i = 0;
+        int l = 0;
+        int r = 0;
+
+        while (l < leftSize && r < rightSize) {
+            if(leftArray[l] < rightArray[r]) {
+                array[i++] = leftArray[l++]
+            }
+            else {
+                array[i++] = rightArray[r++]
+            }
+        }
+        while (l < leftSize) {
+            array[i++] = leftArray[l++];
+        }
+        while (r < rightSize) {
+            array[i++] = rightArray[r++];
+        }
+    }
 }
