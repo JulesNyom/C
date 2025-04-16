21ff56ec463eac3a26e55dff2ed5580bf8cbff69
JulesNyom <julesnyom@gmail.com>
2025-04-16 07:54:07 +0200

    did binary, dont remember how to create an object

--- java_training/src/Algos.java
+++ java_training/src/Algos.java
@@ -53,29 +53,29 @@ public class Algos {
     }
 
     public int linear (int[] array, int target) {
-        for (int i = 0; i<array.length; i++) {
+        for (int i =0; i<array.length; i++) {
             if (target == array[i]) {
                 return i;
             }
         }
         return -1;
     }
+public int binary (int[] array, int target) {
+    int left = 0;
+    int right = array.length - 1;
 
-    public int binary (int[] array, int target) {
-        int left = 0;
-        int right = array.length - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (target == array[mid]) {
-                return mid;
-            }
-            else if (target < array[mid]) {
-                right = mid -1;
-            }
-            else {right = mid +1;}
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (target == array[mid]) {
+            return mid;
         }
-
-        return -1;
+        else if (target < array[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
     }
+
+    return -1;
+}
 }
--- java_training/src/Test.java
+++ java_training/src/Test.java
@@ -51,4 +51,25 @@ public class Test {
             array[i++] = rightArray[r++];
         }
     }
+
+    public int binarysearch (int target, int[] array) {
+        int left = 0;
+        int right = array.length -1;
+
+        while (left > right) {
+            int mid = (left + right) / 2;
+
+            if (target == array[mid]) {
+                return mid;
+            }
+            else if (target <= array[mid]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
 }
