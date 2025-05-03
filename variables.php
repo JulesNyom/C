2a53671ef909a1d2057925a88ff139cff3815e85
JulesNyom <julesnyom@gmail.com>
2025-05-03 11:43:18 +0200

    did slight array manipulations

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php
+
+$fruits = ["apple", "banana", "orange"];
+
+echo $fruits[0]. "<br>";
+echo $fruits[1]. "<br>";
+echo $fruits[2]. "<br>";
+
+foreach ($fruits as $fruit) {
+    echo $fruit. "<br>";
+}
+
+
