31b40b29a4b97286eebc31cfa3422f49c393f400
JulesNyom <julesnyom@gmail.com>
2025-05-04 20:40:45 +0200

    Training my php skills

--- variables.php
+++ variables.php
@@ -1,13 +1,3 @@
 <?php
 
-$fruits = ["apple", "banana", "orange"];
-
-echo $fruits[0]. "<br>";
-echo $fruits[1]. "<br>";
-echo $fruits[2]. "<br>";
-
-foreach ($fruits as $fruit) {
-    echo $fruit. "<br>";
-}
-
-
+print_r("i love pasta");
