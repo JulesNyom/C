68c3d67bdd3f6b4a495872ffc6d82e609cd6f8d3
JulesNyom <julesnyom@gmail.com>
2025-05-11 20:32:20 +0200

    finished binary

--- java_training/src/Test.java
+++ java_training/src/Test.java
@@ -9,7 +9,15 @@ public class Test {
 
             int mid = (left + right) / 2;
 
-            if array
+            if (target == array[mid]) {
+                return mid;
+            }
+            else if (target < array[mid]) {
+                right = mid -1;
+            }
+            else {
+                left = mid +1;
+            }
         }
 
         return -1;
