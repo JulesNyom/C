52485ecfd6f46c3926c6991c3726f8a4f3134253
JulesNyom <julesnyom@gmail.com>
2025-05-15 07:37:54 +0200

    Did binary search in python

--- Test.py
+++ Test.py
@@ -1,17 +1,13 @@
-def binary(target, array):
+def binarysearch (target, array):
     left = 0
-    right = len(array) -1
+    right = len(array)
 
-    while left <= right :
+    while left <= right:
         mid = (left + right) // 2
+
         if target == array[mid]:
             return mid
         elif target < array[mid]:
             right = mid -1
-        else : 
-            left = mid + 1
+        else: left = mid + 1
     return -1
-
-sorted_array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
-
-print(binary(38, sorted_array))
